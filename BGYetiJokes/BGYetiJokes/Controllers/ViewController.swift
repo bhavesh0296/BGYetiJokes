a7fa532a59fb2f1087d2f0f7034c170c12dac6c5
Bhavesh <bhavesh.gupta@techtreeit.com>
2021-08-28 17:29:26 +0530

    included Flyweight Design Pattern

--- BGYetiJokes/BGYetiJokes/Controllers/ViewController.swift
+++ BGYetiJokes/BGYetiJokes/Controllers/ViewController.swift
@@ -7,26 +7,44 @@
 //
 
 import UIKit
+import YetiTheme
 
 public class ViewController: UIViewController {
 
-  // MARK: - Instance properties
-  public let jokeProvider: JokeProvider = JokeProvider(fileName: "jokes")
+    // MARK: - Instance properties
+    public let jokeProvider: JokeProvider = JokeProvider(fileName: "jokes")
 
-  // MARK: - Outlets
-  @IBOutlet public var textLabel: UILabel!
-  @IBOutlet public var segmentedControl: UISegmentedControl!
-
-  // MARK: - Actions
-  @IBAction public func segmentedControlValueChanged(_ sender: UISegmentedControl) {
-
-  }
-
-  @IBAction public func newJokePressed(_ sender: UIButton) {
-    guard let joke = jokeProvider.jokes.randomElement() else {
-      return
+    // MARK: - View Lifecycle
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        textLabel.font = Fonts.small
     }
 
-    textLabel.text = joke.question + "\n\n" + joke.answer
-  }
+    // MARK: - Outlets
+    @IBOutlet public var textLabel: UILabel!
+    @IBOutlet public var segmentedControl: UISegmentedControl!
+
+    // MARK: - Actions
+    @IBAction public func segmentedControlValueChanged(_ sender: UISegmentedControl) {
+
+        switch sender.selectedSegmentIndex {
+        case 0:
+            textLabel.font = Fonts.small
+        case 1:
+            textLabel.font = Fonts.medium
+        case 2:
+            textLabel.font = Fonts.large
+        default:
+            textLabel.font = Fonts.small
+        }
+
+    }
+
+    @IBAction public func newJokePressed(_ sender: UIButton) {
+        guard let joke = jokeProvider.jokes.randomElement() else {
+            return
+        }
+
+        textLabel.text = joke.question + "\n\n" + joke.answer
+    }
 }
